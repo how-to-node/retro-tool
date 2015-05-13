a8352e4674b151f7ddd7fdcc95ab87e73a750326
Facundo Jauregui <jaureguifacundo@gmail.com>
2015-05-12 22:53:01 -0300

    Adding user to views

--- routes/setup.js
+++ routes/setup.js
@@ -1,3 +1,5 @@
+var sessionsManager = require('../model/session/manager');
+
 // Mapping paths with routers
 var routes = {
     '/': require('./main'),
@@ -6,6 +8,17 @@ var routes = {
 };
 
 module.exports = function(app) {
+    // Making user available to all views
+    app.use(function(req, res, next) {
+        var user = null;
+        if (req.session) {
+            user = sessionsManager.getUser(req.session.userKey);
+        }
+        res.locals.user = user;
+        next();
+    });
+
+    // Routes mapping
     Object.keys(routes).forEach(function(path) {
         app.use(path, routes[path]);
     });
